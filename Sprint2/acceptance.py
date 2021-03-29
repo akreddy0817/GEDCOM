8ebd2538c745b5e0c1c07d82f058970b75671919
Akhilesh Reddy <65865843+akreddy0817@users.noreply.github.com>
2021-03-29 12:52:45 -0400

    Add files via upload

---
+++ Sprint2/acceptance.py
@@ -0,0 +1,33 @@
+from gedTable import printTable
+from gedTable import processGEDCOM
+from userStory5 import userStory05
+from userStory6 import userStory06
+from userStory7 import userStory07
+from userStory8 import userStory08
+
+def sprint2_results():
+	
+	idict,famdict = processGEDCOM(fileName)
+	indiTable,familyTable= printTable(idict, famdict)
+	errors = []
+	errors.extend(userStory05("sprint2_Reddy.ged"))
+	errors.extend(userStory06("sprint2_Reddy.ged"))
+	errors.extend(userStory07("sprint2_Reddy.ged"))
+	errors.extend(userStory08("sprint2_Reddy.ged"))
+
+	for e in errors:
+		print(e)
+
+	with open('acceptance_results.txt','w') as file:
+		file.write('Individuals Information--------------->\n')
+		file.write(indiTable.get_string())
+		file.write("\n\n\n")
+		file.write('Family Information------------>\n')
+		file.write(familyTable.get_string())
+		file.write("\n\n")
+		for e in errors:
+			file.write(e+"\n")
+
+
+if __name__ == "__main__":
+    sprint2_results()
