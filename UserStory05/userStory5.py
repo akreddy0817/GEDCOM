a8a3688bf89aee82f07bd7c03ab17e91a8a6ee22
Akhilesh Reddy <65865843+akreddy0817@users.noreply.github.com>
2021-03-28 23:44:59 -0400

    Add files via upload

--- UserStory05/userStory5.py
+++ UserStory05/userStory5.py
@@ -9,40 +9,40 @@ from dateutil import relativedelta as rdelta
 
 
 def userStory05(file):
-    indiDict,famDict = processGEDCOM(file)
+    i_dict,family = processGEDCOM(file)
     wife_status = False
     men_status = False
-    resultList = list()
+    errors = list()
 
 
-    for key in famDict:
-        family = famDict[key]
-        familyID = family.Get_ID()
+    for key in family:
+        family = family[key]
+        fam_id = family.Get_ID()
 
-        if(famDict[key] != 'NA'):
-            husbandID = str(famDict[familyID].Get_husbandID())
-            wifeID = str(famDict[familyID].Get_wifeID())
-            if(husbandID != 'NA' and wifeID != 'NA'):
-                if(indiDict[husbandID].Get_death() != 'NA'):
-                    husbandDeath = indiDict[husbandID].Get_death()
+        if(family[key] != 'NA'):
+            h_id = str(family[fam_id].Get_h_id())
+            w_id = str(family[fam_id].Get_w_id())
+            if(h_id != 'NA' and w_id != 'NA'):
+                if(i_dict[h_id].Get_death() != 'NA'):
+                    husbandDeath = i_dict[h_id].Get_death()
                     men_status = True
                     
-                if(indiDict[wifeID].Get_death() != 'NA'):
-                    wifeDeath = indiDict[wifeID].Get_death()
+                if(i_dict[w_id].Get_death() != 'NA'):
+                    wifeDeath = i_dict[w_id].Get_death()
                     wife_status = True
                     
-                if(famDict[familyID].Get_married() != "NA"):
-                    marriedDate = famDict[familyID].Get_married()
+                if(family[fam_id].Get_married() != "NA"):
+                    marriedDate = family[fam_id].Get_married()
 
                 if(wife_status and marriedDate>wifeDeath):
-                    result_1_str = f"ERROR: FAMILY: US05: The wedding occurs after wife death. Wedding Date: " + str(famDict[familyID].Get_married()) + " Wife Death: " + str(wifeDeath)
-                    resultList.append(result_1_str)
+                    result_1_str = f"ERROR: FAMILY: US05: The wedding occurs after wife death. Wedding Date: " + str(family[fam_id].Get_married()) + " Wife Death: " + str(wifeDeath)
+                    errors.append(result_1_str)
 
                 if(men_status and marriedDate>husbandDeath):
-                    result_1_str = f"ERROR: FAMILY: US05: The wedding occurs after husband death. Wedding Date: " + str(famDict[familyID].Get_married()) + " Husband Death: " + str(husbandDeath)
-                    resultList.append(result_1_str)
+                    result_1_str = f"ERROR: FAMILY: US05: The wedding occurs after husband death. Wedding Date: " + str(family[fam_id].Get_married()) + " Husband Death: " + str(husbandDeath)
+                    errors.append(result_1_str)
 
-    return resultList
+    return errors
 
 
 if __name__ == "__main__":
