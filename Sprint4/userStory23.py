af57067981027347d1f1d8875f4903267d526b1e
Akhilesh Reddy <65865843+akreddy0817@users.noreply.github.com>
2021-04-26 01:43:19 -0400

    Add files via upload

---
+++ Sprint4/userStory23.py
@@ -0,0 +1,41 @@
+from individual import individual
+from gedTable import processGEDCOM
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+import datetime
+from dateutil import relativedelta as rdelta
+
+
+
+def userStory23(file):
+
+    indiDict, famDict = processGEDCOM(file)
+    indiList = []
+    resultList = list()
+
+    for index in indiDict:
+        indiList += [index]
+
+    count = 0
+    while count <= len(indiList)-2:
+        tempIndi1 = indiDict[indiList[count]]
+
+        temp = count + 1
+        while temp <= len(indiList)-1:
+            tempIndi2 = indiDict[indiList[temp]]
+            if tempIndi1.Get_name() == tempIndi2.Get_name() and tempIndi1.Get_birthday() == tempIndi2.Get_birthday():
+                indi_1 = tempIndi1.Get_ID()
+                indi_2 = tempIndi2.Get_ID()
+                indiBirth_2 = tempIndi2.Get_birthday()
+                indiName_2 = tempIndi1.Get_name()
+                resultList.append(f"ERROR: INDIVIDUAL: US23: Two individuals have duplicate names and birthdays [{tempIndi1.Get_name()}, {tempIndi2.Get_birthday()}].")
+            temp += 1
+        count += 1
+
+    resultList.sort()
+    
+    return resultList
+if __name__ == "__main__":
+    print(userStory23("sprint4_Reddy.ged"))
---
+++ Sprint4/userStory24.py
@@ -0,0 +1,41 @@
+from individual import individual
+from gedTable import processGEDCOM
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+import datetime
+from dateutil import relativedelta as rdelta
+
+
+
+def userStory24(file):
+
+    indiDict, famDict = processGEDCOM(file)
+    famList = []
+    resultList = list()
+
+    for index in famDict:
+        famList += [index]
+
+    count = 0
+    while count <= len(famList)-2:
+        tempFam1 = famDict[famList[count]]
+        tempFam1_Husb = tempFam1.Get_husbandName()
+        tempFam1_Wife = tempFam1.Get_wifeName()
+        temp = count + 1
+        while temp <= len(famList)-1:
+            tempFam2 = famDict[famList[temp]]
+            tempFam2_Husb = tempFam2.Get_husbandName()
+            tempFam2_Wife = tempFam2.Get_wifeName()
+            if tempFam1_Husb == tempFam2_Husb and tempFam1_Wife == tempFam2_Wife:
+                fam_1 = tempFam1.Get_ID()
+                fam_2 = tempFam2.Get_ID()
+                resultList.append(f"ERROR: FAMILY: US24: Two families [{fam_1}, {fam_2}] have duplicate spouses [{tempFam1_Husb}, {tempFam2_Wife}].")
+            temp += 1
+        count += 1
+
+    resultList.sort()
+    return resultList
+if __name__ == "__main__":
+    print(userStory24("sprint4_Reddy.ged"))
